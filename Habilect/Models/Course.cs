3764674c5cd0f2289592697c930270002b4f218f
Warchiefs <ialexmur@gmail.com>
2015-03-22 14:00:14 +0300

    Models (ver1)
    Add Models:
    # Course
    # Motion

---
+++ Habilect/Models/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Habilect.Models
+{
+    public class Course
+    {
+        public Course()
+        {
+            Motions = new HashSet<Motion>();
+        }
+
+        public int Id { get; set; }
+        public int Order { get; set; }
+        public string Name { get; set; }
+        public List<int> MotionsId { get; set; }
+
+        public ICollection<Motion> Motions { get; set; }
+    }
+
+
+}
